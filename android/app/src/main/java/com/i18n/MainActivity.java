75ed61cbf387716ec2e6562b13bfdc9bd7b233bb
Leonardo Lima Coelho <ryszol@protonmail.com>
2022-01-15 09:41:23 -0400

    Setup react-native-screens

--- android/app/src/main/java/com/i18n/MainActivity.java
+++ android/app/src/main/java/com/i18n/MainActivity.java
@@ -1,5 +1,6 @@
 package com.i18n;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "i18n";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
